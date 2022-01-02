1b4ad8c260ed9e5361902af9ebf0b8e12a298bbf
Sebastian <19Sebastian95@gmx.de>
2022-01-02 17:00:44 +0100

    initial react files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        We are using Node.js <span id="node-version"></span>,
+        Chromium <span id="chrome-version"></span>,
+        and Electron <span id="electron-version"></span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
