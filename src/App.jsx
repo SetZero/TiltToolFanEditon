f0bcb5265024e22bee50b2e84128efa1beae9b61
Sebastian <19Sebastian95@gmx.de>
2022-01-02 17:16:51 +0100

    Added react material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CircularProgress color="secondary" />
         We are using Node.js <span id="node-version"></span>,
         Chromium <span id="chrome-version"></span>,
         and Electron <span id="electron-version"></span>
