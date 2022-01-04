e02f3123d0fe83e271cba4fbda33e0f2b2cd670b
unknown <gent.tobias@gmail.com>
2022-01-04 13:24:36 +0100

    feature(root) - added 'dotenv' dependency and global .env file

--- src/routes/MainApp.jsx
+++ src/routes/MainApp.jsx
@@ -1,10 +1,13 @@
 import './styles/MainApp.css';
 
+var riot_api_key = process.env.REACT_APP_API_KEY;
+
 function MainApp() {
   return (
     <div className="MainApp">
       <header className="MainApp-header">
         <h1>MainApp</h1>
+        <p>{riot_api_key}</p>
       </header>
     </div>
   );
